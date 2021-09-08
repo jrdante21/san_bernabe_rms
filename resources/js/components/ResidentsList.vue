132a4e5efb41116d18d2ac195652ef429a212008
jrdante21 <danteduenas10@gmail.com>
2021-09-08 17:59:21 +0800

    second commit

--- resources/js/components/ResidentsList.vue
+++ resources/js/components/ResidentsList.vue
@@ -106,7 +106,6 @@ export default {
                 .then((res)=>{
                     this.residentsLoading = false
                     this.residents = res.data.data
-
                     this.pagination = res.data
                 })
                 .catch((err)=>{
